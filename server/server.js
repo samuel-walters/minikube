915e9f30885ce8886fb68bd51a74222f72a41b7d
samuel-walters <swalters@spartaglobal.com>
2022-10-09 19:27:06 +0100

    use mongo version 6 instead

--- server/server.js
+++ server/server.js
@@ -1,63 +1,63 @@
 const express = require('express');
-const mongoose = require("mongoose");
+const { MongoClient } = require('mongodb');
 const bodyParser = require('body-parser');
 
-mongoose.Promise = global.Promise;
-mongoose.connect("mongodb://mongo-0.mongo,mongo-1.mongo,mongo-2.mongo:27017/my-db");
-const nameSchema = new mongoose.Schema({
-  userID: {
-    type: Number,
-    unique: true,
-    required: true
-  },
-  firstName: {
-    type: String,
-    required: true
-  },
-  lastName: {
-    type: String,
-    required: true
-  },
-  age: {
-    type: Number,
-    required: true
-  },
-  date: {
-    type: Date,
-    default: Date.now
+const client = new MongoClient("mongodb://mongo-0.mongo,mongo-1.mongo,mongo-2.mongo,mongo-3.mongo,mongo-4.mongo,mongo-5.mongo,mongo-6.mongo:27017");
+
+async function main(doc, type, inputID) {
+  // Use connect method to connect to the server
+  await client.connect();
+  console.log('Connected successfully to server');
+  const db = client.db("my-db");
+  const collection = db.collection('users');
+  if (type === "post") {
+    await collection.createIndex({ "UserID": 1 }, { unique: true })
+    await collection.insertOne(doc);
+    console.log(`A document was inserted with the UserID: ${doc.UserID}`);
+  } else if (type === "get") {
+    const findResult = await collection.find({}).toArray();
+    console.log('Found documents =>', findResult);
+    return findResult;
+  } else if (type === "delete") {
+    const deleteResult = await collection.deleteOne({ UserID: inputID });
+    console.log('Deleted a document => ', deleteResult);
   }
-});
-const User = mongoose.model("User", nameSchema);
+  return 'done.';
+}
 
-const server = express();
+app = express()
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json());
+app.use(express.static(__dirname + '/public'));
+app.set("view engine", "ejs");
 
-// Tidies up the request object.
-server.use(bodyParser.urlencoded({ extended: true }))
-server.use(bodyParser.json());
-
-server.use(express.static(__dirname + '/public'));
-server.set("view engine", "ejs");
-
-//post
-server.post("/addname", (req, res) => {
-  const insertedData = new User(req.body);
+// post
+app.post("/addname", async (req, res) => {
+  const insertedData = req.body;
   const checkNumber = /^\d+$/;
   const checkString = /^[A-Za-z]+$/;
   const age = insertedData.age;
-  if (checkString.test(insertedData.firstName) && checkString.test(insertedData.secondName)) {
+  if (checkString.test(insertedData.firstName) && checkString.test(insertedData.lastName)) {
     if (age != null && age >= 18 && age <= 100 && checkNumber.test(age)) {
-      if (checkNumber.test(insertedData.userID)) {
-        insertedData.save(function (err) {
-          if (err) {
-            res.render("error.ejs");
-          }
-          res.render("successful.ejs");
-      });
-      } else {
-        res.render("invalid_id.ejs");
-      }
+        if (checkNumber.test(insertedData.userID)) {
+            const doc = {
+                UserID: insertedData.userID,
+                firstName: insertedData.firstName,
+                lastName: insertedData.lastName,
+                age: insertedData.age
+            }
+            let errorOccurred = 0;
+            await main(doc, "post").then(console.log).catch(error => errorOccurred = 1).finally(() => client.close());
+            if (errorOccurred == 0) {
+              res.render("successful.ejs");
+            } else {
+              res.render("error.ejs");
+            }
+        } else {
+            res.render("invalid_id.ejs");
+        }
     } else {
-      res.render("invalid_age.ejs");
+        res.render("invalid_age.ejs");
     }
   } else {
     res.render("invalid_name.ejs");
@@ -65,13 +65,13 @@ server.post("/addname", (req, res) => {
 });
 
 // Get
-server.get("/users", async(req, res) => {
-  const readData = await User.find({});
+app.get("/users", async (req, res) => {
+  const readData = await main({}, "get");
   res.render("list.ejs", {userList: readData})
 });
 
 // Delete
-server.get("/delete", (req, res) => {
+app.get("/delete", async (req, res) => {
   res.render("delete.ejs");
   var stringed = JSON.stringify(req.query, null, 2);
   var objectValue = JSON.parse(stringed);
@@ -79,16 +79,8 @@ server.get("/delete", (req, res) => {
   if (passedID === undefined) {
     console.log("passedID is undefined.");
   } else {
-    console.log(passedID);
-    User.remove({"userID":passedID}, function(err,result) {
-      if (err) {
-        console.log(err);
-      } else {
-        console.log(result);
-      }
-    });
-}});
-
-server.listen(3000, () => {
-  console.log(`Server listening on port 3000!`)
+    await main({}, "delete", passedID);
+  }
 });
+
+app.listen(3000, () => console.log("Server started and listening on port 3000!"));
